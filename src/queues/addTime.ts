7f2bd30adfaac1688646e96174e1f834ee97650a
Matheus <matheus.giordani@compasso.com.br>
2021-10-25 18:19:57 -0300

    feat: add time

--- src/queues/addTime.ts
+++ src/queues/addTime.ts
@@ -1,9 +1,28 @@
 import { ConsumeMessage } from 'amqplib';
+import { add } from 'date-fns';
 import { AddTime } from '../interfaces/add-time';
 import { ParkModel } from '../schemas/park.schema';
 import { UserModel } from '../schemas/user.schema';
 
-export const addTime = async (msg: ConsumeMessage) => {
-  console.log(`ADD_TIME -- Message received msg: ${msg.content.toString()}`);
+const processor = async (msg: ConsumeMessage) => {
+  console.log(`ADD_TIME -- Message received msg: ${msg.content.toString()}\n`);
   const { parkId, chargedAmount, parkedTime } = JSON.parse(msg.content.toString()) as AddTime;
+
+  const park = await ParkModel.findById(parkId);
+  park.parkEndTime = add(park.parkEndTime, { minutes: parkedTime });
+
+  const user = await UserModel.findOne({ cpf: park.vehicleOwnerDocument });
+  user.pay(chargedAmount);
+
+  await Promise.all([user.save(), park.save()]);
 };
+
+export const addTime = { processor, name: 'ADD_TIME' };
+
+/*
+{
+  "parkId": "617604c0d68a6d43d31e3774",
+  "chargedAmount": 0.6,
+  "parkedTime": 30
+}
+*/
